821767c3c7e714e325a3d963ce7a3b4dfe7b8d64
mokiros <mokiros@yandex.ru>
2024-01-26 01:18:40 +0300

    new test scripts with nodejs server

---
+++ test_proxy/src/index.ts
@@ -0,0 +1,73 @@
+import express from "express"
+import * as pty from 'node-pty'
+
+function sleep(n: number): Promise<void> {
+	return new Promise((resolve) => {
+		setTimeout(resolve, n)
+	})
+}
+
+class ShellSession {
+	private readonly proc
+
+	private out = ''
+
+	constructor(w: number, h: number) {
+		this.proc = pty.spawn('zsh', [], {
+			name: 'xterm-256color',
+			cols: w,
+			rows: h,
+			cwd: process.env.HOME,
+			env: process.env,
+		})
+		this.proc.onData((data) => {
+			this.out += data
+		})
+	}
+
+	public async update(input: string) {
+		if (input != '') {
+			this.proc.write(input)
+		}
+		await sleep(10)
+		const data = this.out
+		this.out = ''
+		return data
+	}
+}
+
+const Sessions = new Map<string, ShellSession>()
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.get('/session', (req, res) => {
+	const id = crypto.randomUUID()
+	const w = Number(req.query['w'] ?? 60)
+	const h = Number(req.query['h'] ?? 40)
+	const session = new ShellSession(w, h)
+	Sessions.set(id, session)
+	res.json({ session: id, w, h })
+})
+
+app.post('/session/:sessionid', async (req, res) => {
+	const id = req.params.sessionid
+	const session = Sessions.get(id)
+	if (!session) {
+		res.json({ error: 'Invalid session id' })
+		return
+	}
+	const body: { body: string } = req.body
+	const out = await session.update(body.body)
+	res.send(out)
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
