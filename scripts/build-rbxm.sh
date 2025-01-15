a4fe62ff5676714fe5a6b094dfe9915f37de2f51
mokiros <mokiros@mokiros.dev>
2025-01-15 22:39:24 +0300

    chore(scripts): Moved commands to package.json

--- scripts/build-rbxm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd $SCRIPT_DIR/../
-darklua process -c ./darklua.json src out
-rojo build --output ./luau_term.rbxm ./default.project.json
